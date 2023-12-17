b9d2fe9052385fb070a5daf0d2d044c5c1d6085f
Daniil <oleinik.d_78@mail.ru>
2023-12-17 22:58:51 +0300

    Ver 1.1

--- src/main/java/org/program/Main.java
+++ src/main/java/org/program/Main.java
@@ -2,16 +2,10 @@ package org.program;
 
 import java.io.IOException;
 
-//Олейник Даниил Николаевич 02.11.1998 89288815770 m
 public class Main {
     public static void main(String[] args) throws IOException {
     PersonalData personalData = new PersonalData();
-        System.out.println(personalData.getDate());
-        System.out.println(personalData.getFio());
-        System.out.println(personalData.getGender());
-        System.out.println(personalData.getNumber());
-
-        WriteToFile writeToFile = new WriteToFile(personalData.getFio().get(0));
-        writeToFile.recording(personalData);
+    WriteToFile writeToFile = new WriteToFile(personalData.getFio().get(0));
+    writeToFile.recording(personalData);
     }
 }
